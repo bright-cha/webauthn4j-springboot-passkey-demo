c92b5b4cb95ee7e1b9159a3b2643a92b5e74bdc1
brightCha <ckguscjf03@gmail.com>
2025-01-19 21:38:54 +0900

    feat: webAuthn4j depandency downgrade and userService code remove

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,21 +1,32 @@
 package com.example.demo.config;
 
 import com.webauthn4j.WebAuthnManager;
+import com.webauthn4j.data.AttestationConveyancePreference;
+import com.webauthn4j.data.PublicKeyCredentialParameters;
+import com.webauthn4j.data.PublicKeyCredentialType;
+import com.webauthn4j.data.attestation.statement.COSEAlgorithmIdentifier;
+import com.webauthn4j.springframework.security.WebAuthnAuthenticationProvider;
 import com.webauthn4j.springframework.security.config.configurers.WebAuthnLoginConfigurer;
 import com.webauthn4j.springframework.security.credential.WebAuthnCredentialRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.webauthn.api.*;
-import org.springframework.security.web.webauthn.authentication.WebAuthnAuthenticationProvider;
+import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.util.List;
 
@@ -23,16 +34,19 @@ import java.util.List;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
-    public WebAuthnAuthenticationProvider webAuthnAuthenticationProvider(WebAuthnAuthenticatorService authenticatorService, WebAuthnManager webAuthnManager){
+    public WebAuthnAuthenticationProvider webAuthnAuthenticationProvider(WebAuthnCredentialRecordService authenticatorService, WebAuthnManager webAuthnManager){
         return new WebAuthnAuthenticationProvider(authenticatorService, webAuthnManager);
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService){
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
         return daoAuthenticationProvider;
     }
 
@@ -42,63 +56,81 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public WebAuthnAuthenticationProvider webAuthnAuthenticationProvider(WebAuthnCredentialRecordService webAuthnCredentialRecordService, WebAuthnManager webAuthnManager){
-        return new WebAuthnAuthenticationProvider(webAuthnCredentialRecordService, webAuthnManager);
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService){
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-        return daoAuthenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(List<AuthenticationProvider> providers){
-        return new ProviderManager(providers);
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> {
+            // ignore static resources
+            web.ignoring().requestMatchers(
+                    "/favicon.ico",
+                    "/js/**",
+                    "/css/**",
+                    "/webjars/**");
+        };
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         // WebAuthn Login
-        http.apply(WebAuthnLoginConfigurer.webAuthnLogin())
-                .loginPage("/login")
-                .usernameParameter("username")
-                .passwordParameter("rawPassword")
-                .credentialIdParameter("credentialId")
-                .clientDataJSONParameter("clientDataJSON")
-                .authenticatorDataParameter("authenticatorData")
-                .signatureParameter("signature")
-                .clientExtensionsJSONParameter("clientExtensionsJSON")
-                .loginProcessingUrl("/login")
-                .rpId("example.com")
-                .attestationOptionsEndpoint()
-                .attestationOptionsProvider(attestationOptionsProvider)
-                .processingUrl("/webauthn/attestation/options")
-                .rp()
-                .name("example")
-                .and()
-                .pubKeyCredParams(
-                        new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, COSEAlgorithmIdentifier.ES256),
-                        new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, COSEAlgorithmIdentifier.RS1)
-                )
-                .authenticatorSelection()
-                .authenticatorAttachment(AuthenticatorAttachment.CROSS_PLATFORM)
-                .residentKey(ResidentKeyRequirement.PREFERRED)
-                .userVerification(UserVerificationRequirement.PREFERRED)
-                .and()
-                .attestation(AttestationConveyancePreference.DIRECT)
-                .extensions()
-                .credProps(true)
-                .uvm(true)
-                .and()
-                .assertionOptionsEndpoint()
-                .assertionOptionsProvider(assertionOptionsProvider)
-                .processingUrl("/webauthn/assertion/options")
-                .rpId("example.com")
-                .userVerification(UserVerificationRequirement.PREFERRED)
-                .and()
-                .authenticationManager(authenticationManager);
+        http.with(WebAuthnLoginConfigurer.webAuthnLogin(), (customizer)-> {
+            customizer
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/login")
+                    .attestationOptionsEndpoint()
+                    .rp()
+                    .name("WebAuthn4J Spring Security Sample")
+                    .and()
+                    .pubKeyCredParams(
+                            new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, COSEAlgorithmIdentifier.ES256),
+                            new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, COSEAlgorithmIdentifier.RS1)
+                    )
+                    .attestation(AttestationConveyancePreference.NONE)
+                    .extensions()
+                    .uvm(true)
+                    .credProps(true)
+                    .extensionProviders()
+                    .and()
+                    .assertionOptionsEndpoint()
+                    .extensions()
+                    .extensionProviders();
+        });
+
+        http.headers(headers -> {
+            headers.contentSecurityPolicy(csp ->
+                    csp.policyDirectives("default-src 'self'; frame-ancestors *; publickey-credentials-get *;")
+            );
+
+            headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
+        });
+
+        // Authorization
+        http.authorizeHttpRequests(authz -> authz
+                .requestMatchers(HttpMethod.GET, "/login").permitAll()
+                .requestMatchers(HttpMethod.GET, "/signup").permitAll()
+                .requestMatchers(HttpMethod.POST, "/signup").permitAll()
+                .anyRequest().access(getWebExpressionAuthorizationManager("@webAuthnSecurityExpression.isWebAuthnAuthenticated(authentication) || hasAuthority('SINGLE_FACTOR_AUTHN_ALLOWED')"))
+        );
+
+        http.exceptionHandling(customizer -> {
+            customizer.accessDeniedHandler((request, response, accessDeniedException) -> response.sendRedirect("/login"));
+        });
+
+
+        http.authenticationManager(authenticationManager);
+
+        // As WebAuthn has its own CSRF protection mechanism (challenge), CSRF token is disabled here
+        http.csrf(customizer -> {
+            customizer.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            customizer.ignoringRequestMatchers("/webauthn/**");
+        });
+
+        return http.build();
+
+    }
+
+    private WebExpressionAuthorizationManager getWebExpressionAuthorizationManager(final String expression) {
+        DefaultHttpSecurityExpressionHandler expressionHandler = new DefaultHttpSecurityExpressionHandler();
+        expressionHandler.setApplicationContext(applicationContext);
+        WebExpressionAuthorizationManager authorizationManager = new WebExpressionAuthorizationManager(expression);
+        authorizationManager.setExpressionHandler(expressionHandler);
+        return authorizationManager;
     }
 }
